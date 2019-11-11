02da519e13ae20657ca979425ef421b278ff5e73
Nils Hoffmann <nils.hoffmann@god.de>
2019-11-11 16:12:10 +0100

    Initial Version of application with bootstrap and some data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector:    'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls:   ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'god-herbs-and-spices';
+  public herbsAndSpices = [
+    {
+      name:      'Lemon Thyme',
+      type:      'Herb',
+      latinName: 'Thymus citriodorus'
+    },
+    {
+      name:      'Basil',
+      type:      'Herb',
+      latinName: 'Ocimum basilicum'
+    },
+    {
+      name:      'Spearmint',
+      type:      'Herb',
+      latinName: 'Mentha spicata'
+    },
+    {
+      name:      'Rosemary',
+      type:      'Herb',
+      latinName: 'Salvia rosmarinus'
+    },
+    {
+      name:      'Black Pepper',
+      type:      'Spice',
+      latinName: 'Piper nigrum'
+    },
+    {
+      name:      'Ginger',
+      type:      'Spice',
+      latinName: 'Zingiber officinale'
+    }
+  ];
 }
